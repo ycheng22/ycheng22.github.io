908270ae1f8bf62a6227db970813fd79addcbd0a
ycheng22 <ycheng22@hotmail.com>
2025-10-23 20:03:54 -0400

    chore: extract templates/styles and add built docs for GitHub Pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,8 @@ import { FooterComponent } from './components/footer/footer.component';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
-  template: `
-    <div class="min-h-screen flex flex-col">
-      <app-navbar></app-navbar>
-      <main class="flex-1">
-        <router-outlet></router-outlet>
-      </main>
-      <app-footer></app-footer>
-    </div>
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-personal-website';
