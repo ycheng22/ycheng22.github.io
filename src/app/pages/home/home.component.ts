722084f8f062b8d76792673637d39bd69a581333
ycheng22 <ycheng22@hotmail.com>
2026-01-18 14:25:42 -0500

    minor changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -32,4 +32,11 @@ export class HomeComponent {
     );
   }
 
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
 }
